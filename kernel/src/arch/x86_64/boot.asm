b7d7b25c009dd33df8ddff7816538477045560af
Travis Mundy <mundyt4@mymacewan.ca>
2024-01-06 23:12:20 -0700

    Boot enable a20

--- kernel/src/arch/x86_64/boot.asm
+++ kernel/src/arch/x86_64/boot.asm
@@ -11,24 +11,38 @@
 ORG 0x7c00
 BITS 16
 
-
+stage_1:
+  cli               ; disable interrupts
   mov ax, 0         ; setup segments
   mov ds, ax        ; init the data segment
   mov es, ax        ; init the extra segment
   mov ss, ax        ; setup stack
   mov sp, 0x7c00    ; stack grows downwards from 0x7C00
+  call enable_a20
+  jmp intro
 
+; A20 line represents the 21st bit
+; the fast A20 Gate
+enable_a20:
+  
+  in al, 0x92      ; read the status from the system control port
+  or al, 0x02      ; set the second bit to 1 to enable A20
+  out 0x92, al     ; write the the modified value to back to the port
+  sti              ; re-enable interrupts
+  ret              ; return 
+
+intro:
   mov si, hipp
 
-  call print
+  call print_string
 
-  print:
+  print_string:
     lodsb           ; get byte from SI
     or al, al       ; logical od al by itself
     jz .done        ; if the result is 0 jump to done
     mov ah, 0x0E    ; move 0x0E into ah
     int 0x10        ; otherwise, print out the char
-    jmp print       ; jump to the top of the function
+    jmp print_string; jump to the top of the function
     .done:
       ret
 
