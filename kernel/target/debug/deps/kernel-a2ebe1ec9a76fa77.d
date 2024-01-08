fa4815073aed9245f2b8bcd398bff01320256ad4
Travis <mundyt4@mymacewan.ca>
2024-01-07 21:53:39 -0700

    Update tomls

---
+++ kernel/target/debug/deps/kernel-a2ebe1ec9a76fa77.d
@@ -0,0 +1,9 @@
+/home/mundy/Documents/code/HippOS/kernel/target/debug/deps/libkernel-a2ebe1ec9a76fa77.rmeta: src/main.rs Cargo.toml
+
+/home/mundy/Documents/code/HippOS/kernel/target/debug/deps/kernel-a2ebe1ec9a76fa77.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-W__CLIPPY_HACKERY__clippy::all__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::unwrap_used__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
